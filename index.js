ec7d55450b417c313309dc0e17c90550f14c8d4b
08fdange <08fdange@gmail.com>
2020-08-25 20:32:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+
+function findMatching(array, string) {
+    let matching = [];
+    return matching = array.filter(s => {return s.toLowerCase() === string.toLowerCase()});
+}
+
+function fuzzyMatch(array, string) {
+    let matching = [];
+    return matching = array.filter(s => {
+        for(let i = 0; i < string.length; i++) {
+            return s[i] === string[i];
+        }
+    })
+}
+
+function matchName(array, string) {
+    let matching = [];
+    return matching = array.filter(obj => {
+        return obj.name === string;
+    })
+}
