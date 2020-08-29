df7727c4ebc51ea4ee5c47875d490f819b55fdaa
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-08-29 05:56:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driversArray, stringQuery) {
+  return driversArray.filter( possibleMatch =>
+    possibleMatch.toLowerCase() === stringQuery.toLowerCase()
+  )
+}
+
+function fuzzyMatch(source, testString) {
+  return source.filter( possibleMatch =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  )
+}
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string)
+}
+
