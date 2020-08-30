f03c4722d8a6d96857ec84f64d2e324cd1aecdee
Matthew Jones <matts3371@gmail.com>
2020-08-29 19:02:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, name){
+    const matchingDrivers = array.filter(n => {return n.toLowerCase() === name.toLowerCase()});
+    return matchingDrivers;
+}
+
+
+function fuzzyMatch (list, partialName) {
+    let lengthOfName = partialName.length;
+    return list.filter(function (driverName) {
+      return driverName.slice(0, lengthOfName) === partialName;
+    });
+  }
+
+function matchName(list, name){
+    const matchingNames = list.filter(n => {return n.name === name});
+    return matchingNames;
+}
