5705bd08c8a8ccfc8ba627b3f4d1b6940c3115d5
tyler jones <rteyl1690@gmail.com>
2020-09-01 01:34:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, str){
+  return arr.filter(s => {
+    return s.toLowerCase() === str.toLowerCase()
+  })
+}
+
+function fuzzyMatch(arr, str){
+  return arr.filter(s => {
+    return s.startsWith(str)
+  })
+}
+
+function matchName(arr, str) {
+  return arr.filter(obj => obj.name.match(str))
+}
